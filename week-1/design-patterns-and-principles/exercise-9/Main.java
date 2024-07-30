e9059c3aa90ac36f27f58e7bd61d37ca93e68ba1
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-07-31 01:01:03 +0530

    solved the exercise-9 of design-patter week-1.

---
+++ week-1/design-patterns-and-principles/exercise-9/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+        Light light = new Light();
+        RemoteControl remoteControl = new RemoteControl();
+
+        remoteControl.setCommand(new LightOnCommand(light));
+        remoteControl.pressButton();
+
+        remoteControl.setCommand(new LightOffCommand(light));
+        remoteControl.pressButton();
+    }
+}
+
+interface Command {
+    void execute();
+}
+
+class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+class Light {
+    public void turnOn() {
+        System.out.println("Light is on");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is off");
+    }
+}
