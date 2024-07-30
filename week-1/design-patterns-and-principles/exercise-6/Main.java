7143342bcc256256e13ee5f73b0cb67056b01713
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-07-31 00:43:41 +0530

    solved the exercise-6 of design-patter week-1.

---
+++ week-1/design-patterns-and-principles/exercise-6/Main.java
@@ -0,0 +1,58 @@
+public class Main {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("image1.jpg");
+        Image image2 = new ProxyImage("image2.jpg");
+
+        image1.display();
+        image1.display();
+        image2.display();
+        image2.display();
+    }
+}
+
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadFromServer();
+    }
+
+    private void loadFromServer() {
+        System.out.println("Loading image from server: " + filename);
+        // Simulate loading image from server
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying image: " + filename);
+    }
+}
+
+class ProxyImage implements Image {
+    private RealImage realImage;
+    private String filename;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+        }
+        realImage.display();
+    }
+}
+
+
