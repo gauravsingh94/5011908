6844263f3e1519159bde02477a82d4457bd9fbd9
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-08-18 00:15:58 +0530

    done the week-3 exercise 7.

---
+++ week-3/exercise-7/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.example.EmployeeManagementSystem.repository;
+
+import com.example.EmployeeManagementSystem.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Page<Employee> findAll(Pageable pageable);
+
+    Page<Employee> findByDepartmentName(String departmentName, Pageable pageable);
+}
---
+++ week-3/exercise-7/src/main/java/com/example/EmployeeManagementSystem/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.example.EmployeeManagementSystem.service;
+
+import com.example.EmployeeManagementSystem.model.Employee;
+import com.example.EmployeeManagementSystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    //    create and update employee
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    //    get employee by id
+    public Optional<Employee> getEmployee(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    // Get all employees with pagination and sorting
+    public Page<Employee> getAllEmployees(Pageable pageable) {
+        return employeeRepository.findAll(pageable);
+    }
+
+    //   Delete employee by id
+    public boolean deleteEmployeeById(Long id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
